a12a9b88b9543defb195b0d5dd9b2a7a262f14ee
Lucas-Caronni <lcaronni@udesa.edu.ar>
2024-11-23 10:17:36 -0300

    avances USER

--- myapp/controllers/userController.js
+++ myapp/controllers/userController.js
@@ -1,25 +1,37 @@
- const db = require('../database/models/User');
- const bcrypt = require("bcryptjs")
+const db = require('../database/models/User');
+const bcrypt = require("bcryptjs")
 
-module.exports = {
-     register: (req, res) => {
-         res.render('register');
-     },
-     login: (req, res) => {
-         res.render('login');
-       }
-    
-
-       //aca adentro tenemos que poner toda la logica de el modelo de users
-
-       //results
-
-       //loginUser
-
-       //logout
-
-       //conectar todo con las vistas tambien, 
-      
- };
+const userControllers = {
+    register: (req, res) => {
+        res.render('register');
+    },
+    //loginUser
+    login: (req, res) => {
+        res.render('login');
+    },
+    formLogin: function(req, res) {
+        // Esta ruta no renderiza, redirecciona al usuario a home
+        return
+    },
+    formRegister: function(req, res) {
+        // Esto no renderiza, que redirecciona al usuario a login
+        return
+    },
 
 
+    //aca adentro tenemos que poner toda la logica de el modelo de users
+
+    //results
+
+
+    //logout
+    logout: function(req, res) {
+        // Esta ruta no renderiza, redirecciona al usuario a (login o home)
+        // limpiar la sesión y borrar la cookie
+    }
+
+    //conectar todo con las vistas tambien, 
+
+};
+
+module.exports = userControllers;
--- myapp/routes/users.js
+++ myapp/routes/users.js
@@ -3,7 +3,15 @@ const userController = require('../controllers/userController');
 var router = express.Router();
 
 /* GET users listing. */
+// Get
 router.get('/register', userController.register);
 router.get('/login', userController.login);
+
+// Post
+router.post('/register', userController.formRegister);
+router.post('/login', userController.formLogin);
+router.post('/logout', userController.logout);
+
+// Param
 
 module.exports = router;
