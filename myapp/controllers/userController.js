0963c5cc5d6c433bbaeae3b7da178cce779abe3b
Lucas-Caronni <lcaronni@udesa.edu.ar>
2024-11-24 20:14:52 -0300

    Register USER

--- myapp/controllers/userController.js
+++ myapp/controllers/userController.js
@@ -1,4 +1,4 @@
-const db = require('../database/models/User');
+const db = require('../database/models');
 const bcrypt = require("bcryptjs")
 const User = db.User;
 
@@ -21,47 +21,61 @@ const userControllers = {
     login: (req, res) => {
         res.render('login');
     },
-    formLogin: function(req, res) {
-        // Esta ruta no renderiza, redirecciona al usuario a home
-        return
-    },
-    formRegister: function(req, res) {
-        // Esto no renderiza, que redirecciona al usuario a login
-        return
-    },
+    // formLogin: function(req, res) {
+    //     // Esta ruta no renderiza, redirecciona al usuario a home
+    //     return
+    // },
+    // formRegister: function(req, res) {
+    //     // Esto no renderiza, que redirecciona al usuario a login
+    //     return
+    // },
 
 
     //aca adentro tenemos que poner toda la logica de el modelo de users
 
     results: (req, res)=>{
-        let qs = req.query;  //esto en el caso de usar el metodo get
+        // let qs = req.query;
+
         let form = req.body;
+
+           db.User.create(form)
+           .then((result)=>{
+            return res.redirect("/users/login")
+
+           }).catch((err)=>{
+            return console.log(err)
+
+
+           });
         
-        let pass = bcrypt.hashSync(form.password, 10)
-        form.password = pass;
         
-        db.User.create(form)
-        .then(function(results) {
-            return res.redirect('/users/login')
-        })
-        .catch(function(error) {
-            console.log(error);
-        })
+        
+        
+        
+        //esto en el caso de usar el metodo get
+        // let form = req.body;
+        
+        // let pass = bcrypt.hashSync(form.password, 10)
+        // form.password = pass;
+        
+        // db.User.create(form)
+        // .then(function(results) {
+        //     return res.redirect('/users/login')
+        // })
+        // .catch(function(error) {
+        //     console.log(error);
+        // })
 
 
     },
-    loginUser: (req, res)=>{
+    // loginUser: (req, res)=>{
 
-        //llenar con la logica
+    //     //llenar con la logica
 
-    },
-    logout: function(req, res) {
-        req.session.destroy()
-        return res.redirect("/")
     }
-
+    
     //conectar todo con las vistas tambien, 
 
-};
+
 
 module.exports = userControllers;
--- myapp/routes/users.js
+++ myapp/routes/users.js
@@ -5,12 +5,22 @@ var router = express.Router();
 /* GET users listing. */
 // Get
 router.get('/register', userController.register);
+
 router.get('/login', userController.login);
 
+
+
+// result get 
+router.get('/register', userController.results);
+
+// result post 
+router.post('/register', userController.results);
+
+
 // Post
-router.post('/register', userController.formRegister);
-router.post('/login', userController.formLogin);
-router.post('/logout', userController.logout);
+// router.post('/register', userController.formRegister);
+// router.post('/login', userController.formLogin);
+// router.post('/logout', userController.logout);
 
 // Param
 
