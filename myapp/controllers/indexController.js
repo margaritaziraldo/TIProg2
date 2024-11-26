59d3528c64c44b893808185a9f408e274dd0b780
margaritaziraldo <mziraldo@udesa.edu.ar>
2024-11-25 22:56:21 -0300

    form add product

--- myapp/controllers/indexController.js
+++ myapp/controllers/indexController.js
@@ -3,83 +3,103 @@ const Product = db.Product;
 const Op = db.Sequelize.Op;
 
 const indexController = {
-    index: function(req, res) {
+    index: function (req, res) {
         Product.findAll({
-            include: [{association: "usuario_producto"}]
-          })
+            include: [{ association: "usuario_producto" }]
+        })
             .then(prod => {
                 // return res.send(prod)
-              res.render('index', {productos: prod});
+                res.render('index', { productos: prod });
             })
             .catch(error => {
-              console.log(error);
+                console.log(error);
             });
     },
-    add: function(req, res) {
+    add: function (req, res) {
         // si el user esta logueado se renderiza la vista, sino lo mando a login
         if (req.session.usuario) {
-            return res.render('product-add', { error : "" });
+            return res.render('product-add', { error: "" });
         } else {
             return res.redirect('/users/login');
         }
     },
-    formAdd: function(req, res) {
+
+    formAdd: function (req, res) { //post que guarda y procesa la informacion que se relleno en el form (el de arriba)
         let body = req.body;
-
-        let titulo = body.titulo;
-        let descripcion = body.descripcion;
-        // faltan ...
-        
-        // validamos q los campos no estén vacíos
-        if (!titulo || !descripcion) {
-            // para probar
-            console.log("No puede dejar vacíos los campos")
-
-            return res.render('product-add', {error: "No puede dejar vacíos los campos"})
+        if (body.name == '') {
+            res.send("El nombre de tu producto no puede estar vacio")
+        } else if (body.image == '') {
+            res.send("Por favor, ingresa una imagen para tu producto")
+        } else if (body.description == '') {
+            res.send('La descripción no puede estar vacia')
         } else {
-            // mandar a la base de datos la información del producto
-            Product.create({
-                titulo: titulo,
-                descripcion: descripcion,
-            })
-                .then(function(res) {
-                    return res.redirect("/");
+            db.Product.create(body)
+                .then(function (results) {
+                    return res.redirect('/')
                 })
-                .catch(function(err) {
-                    // para probar de nuevo
-                    console.log(err)
-                    return res.render('product-add', {error: "Hubo error"})
+                .catch(function (error) {
+                    console.log(error);
                 })
         }
+
     },
+
+    // formAdd: function(req, res) {
+    //     let body = req.body;
+
+    //     let titulo = body.titulo;
+    //     let descripcion = body.descripcion;
+
+    //     // validamos q los campos no estén vacíos
+    //     if (!titulo || !descripcion) {
+    //         // para probar
+    //         console.log("No puede dejar vacíos los campos")
+
+    //         return res.render('product-add', {error: "No puede dejar vacíos los campos"})
+    //     } else {
+    //         // mandar a la base de datos la información del producto
+    //         Product.create({
+    //             titulo: titulo,
+    //             descripcion: descripcion,
+    //         })
+    //             .then(function(res) {
+    //                 return res.redirect("/");
+    //             })
+    //             .catch(function(err) {
+    //                 // para probar de nuevo
+    //                 console.log(err)
+    //                 return res.render('product-add', {error: "Hubo error"})
+    //             })
+    //     }
+    // },
     detalle: function (req, res) {
         let idProducto = req.params.id;
-        
+
         db.Product.findByPk(idProducto)
-            .then(function(response) {
-                
-                return res.render("product",{ producto: response });
+            .then(function (response) {
+
+                return res.render("product", { producto: response });
             })
-            .catch(function(err) {
+            .catch(function (err) {
                 console.log(err)
             })
     },
-    buscar: function(req, res) {
+    buscar: function (req, res) {
         let nombreProducto = req.query.search;
 
         db.Product.findAll({
             where: {
                 nombre: {
-                    [Op.like] : `%${nombreProducto}%`
+                    [Op.like]: `%${nombreProducto}%`
                 }
             }
         })
-            .then(function(response) {
-                res.render('search-results', {productos: response});
+            .then(function (response) {
+                res.render('search-results', { productos: response });
             })
-            .catch(function(err) {
+            .catch(function (err) {
                 console.log(err);
-                
+
             })
     },
     // todo el codigo de productos aca, findOne(), findByPk()...
